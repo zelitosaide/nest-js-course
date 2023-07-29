95b035c31df7610fdf5121063198c74473a4bf77
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-29 19:50:12 +0200

    Cats

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,12 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Post("login")
-  login(): any {
-    return {};
-  }
-
-  @Get("protected")
-  getHello(): string[] {
+  @Get()
+  getHello(): string {
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { NinjasModule } from "./ninjas/ninjas.module";
-import { UsersModule } from "./users/users.module";
+import { CatsController } from "./cats/cats.controller";
 
 @Module({
-  imports: [NinjasModule, UsersModule],
-  controllers: [AppController],
+  imports: [NinjasModule],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
