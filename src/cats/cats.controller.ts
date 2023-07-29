394a08686734a876080cb4f0d40251deec89b072
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-29 23:01:45 +0200

    Cats

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -3,22 +3,17 @@ import { Controller, Get, Param, Post } from "@nestjs/common";
 @Controller("cats")
 export class CatsController {
   @Post()
-  create(): string {
+  async create(): Promise<string> {
     return "This action adds a new cat";
   }
 
   @Get()
-  findAll(): string {
-    return "This action returns all cats";
+  async findAll(): Promise<any[]> {
+    return [];
   }
 
-  // @Get(":id")
-  // findOne(@Param() params: any): string {
-  //   return `This action returns a #${params.id} cat`;
-  // }
-
   @Get(":id")
-  findOne(@Param("id") id: string): string {
+  async findOne(@Param("id") id: string): Promise<string> {
     return `This action returns a #${id} cat`;
   }
 }
