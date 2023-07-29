5b27f195233e7696f0e5b44b010d3fdebc82d8d8
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-30 00:27:43 +0200

    Cats

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -4,4 +4,12 @@ import { Cat } from './interfaces/cat.interface';
 @Injectable()
 export class CatsService {
   private readonly cats: Cat[] = [];
+
+  create(cat: Cat) {
+    this.cats.push(cat);
+  }
+
+  findAll(): Cat[] {
+    return this.cats;
+  }
 }
