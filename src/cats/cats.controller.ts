69349130dee2312e646aaf8c2d7349a9212341d2
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-29 21:34:45 +0200

    Cats

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, HttpCode } from "@nestjs/common";
+import { Controller, Get, HttpCode, Res } from "@nestjs/common";
 
 @Controller("cats")
 export class CatsController {
-  @Get("breed")
-  @HttpCode(201)
+  // @Get("breed")
+  // @HttpCode(201)
+  @Get()
+  // findAll(@Res() response): string {
   findAll(): string {
     return "This action returns all cats";
+    // return response.status(202).json({ name: "Hello"});
   }
 }
