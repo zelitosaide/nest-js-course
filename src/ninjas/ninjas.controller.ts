0582b7ce0d28292e17e4608b5cce0931987c077e
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-29 11:40:42 +0200

    Ninjas

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -27,7 +27,7 @@ export class NinjasController {
   @Post()
   createNinja(@Body() createNinjaDto: CreateNinjaDto) {
     return {
-      createNinjaDto
+      name: createNinjaDto.name
     };
   }
 
