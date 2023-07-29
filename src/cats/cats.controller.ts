f094aaa50e6fd273655b1c5fddb2a6484fbcf19a
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-29 23:45:05 +0200

    Cats

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Put, Query } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from "@nestjs/common";
 import { CreateCatDto } from "./dto/create-cat.dto";
 import { UpdateCatDto } from "./dto/update-cat.dto";
 
@@ -24,5 +24,10 @@ export class CatsController {
   async update(@Param("id") id: string, @Body() updateCatDto: UpdateCatDto) {
     console.log(updateCatDto);
     return `This action updates a #${id} cat`;
+  }
+
+  @Delete(":id")
+  async remove(@Param("id") id: string) {
+    return `This action removes a #${id} cat`;
   }
 }
