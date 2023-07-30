bc8a49d5563e03893480727ec5227350ad84c5db
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-30 13:16:53 +0200

    Artists

--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -6,6 +6,10 @@ export class ArtistsService {
   private readonly artists: Artist[] = [
     { id: 1, name: "Artist A", genre: "Pop",	country: "United States"},
     { id: 2, name: "Artist B", genre:	"Rock", country: "United Kingdom" },
-    { id: 3, name: "Artist C", genre:	"Hip Hop", country: "Canada"}
+    { id: 3, name: "Artist C", genre:	"Hip Hop", country: "Canada" }
   ];
+
+  findAll(): Artist[] {
+    return this.artists;
+  }
 }
