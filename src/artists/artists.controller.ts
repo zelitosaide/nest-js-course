bff1e579eacccc825939ab9a1532252a39093c0b
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-30 18:02:03 +0200

    Artists

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
 import { Artist } from "./interfaces/artist.interface";
 import { ArtistsService } from "./artists.service";
 import { Album } from "src/albums/interfaces/album.interface";
+import { Playlist } from "src/playlists/interfaces/playlist.interface";
 
 @Controller("artists")
 export class ArtistsController {
@@ -20,5 +21,12 @@ export class ArtistsController {
   @Get(":id/albums")
   async findAlbums(@Param("id", ParseIntPipe) id: number): Promise<Album[]> {
     return this.artistsService.findAlbums(id);
+  }
+
+  @Get(":id/playlists")
+  async findPlaylists(
+    @Param("id", ParseIntPipe) id: number,
+  ): Promise<Playlist[]> {
+    return this.artistsService.findPlaylists(id);
   }
 }
