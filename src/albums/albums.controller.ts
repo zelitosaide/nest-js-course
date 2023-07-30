df367502ecbaca95b0c835bed7971e884ddd2dc8
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-30 13:43:19 +0200

    Albums

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from "@nestjs/common";
+import { AlbumsService } from "./albums.service";
+import { Album } from "./interfaces/album.interface";
 
 @Controller("albums")
 export class AlbumsController {
+  constructor(private albumsService: AlbumsService) {}
+
   @Get()
-  async findAll() {
-    return "Albums";
+  async findAll(): Promise<Album[]> {
+    return this.albumsService.findAll();
   }
 }
