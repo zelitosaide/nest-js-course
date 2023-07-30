03fe0c1797292089e5160eaa156dc31c5d77ff9d
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-30 13:21:35 +0200

    Artists

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { Artist } from './interfaces/artist.interface';
+import { ArtistsService } from './artists.service';
 
 @Controller('artists')
 export class ArtistsController {
+  constructor(private artistsService: ArtistsService) {}
 
   @Get()
   async findAll(): Promise<Artist[]> {
-    return 
+    return this.artistsService.findAll();
   }
 }
