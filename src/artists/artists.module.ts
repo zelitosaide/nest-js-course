6cb7293689a32ee01003d9c3a375cb23b5876c79
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-15 22:16:07 +0200

    Sharing resource

--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AlbumsModule } from 'src/albums/albums.module';
+import { ArtistsController } from './artists.controller';
+import { ArtistsService } from './artists.service';
 
 @Module({
-  imports: [AlbumsModule]
+  imports: [AlbumsModule],
+  controllers: [ArtistsController],
+  providers: [ArtistsService]
 })
 export class ArtistsModule {}
