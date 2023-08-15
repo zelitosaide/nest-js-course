c80676ea78c6195d620f0638f79f721bb592860d
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-15 11:24:24 +0200

    Sharing resource

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,13 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
-// import { FormattedDate } from "./app.interface";
+import { FormattedDate } from "./app.interface";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // @Get()
-  // getHello(): FormattedDate {
-  //   return this.appService.getHello();
-  // }
-
   @Get()
-  getAlbums() {
-    return this.appService.getAlbums();
+  getHello(): FormattedDate {
+    return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,21 +1,14 @@
 import { Injectable } from "@nestjs/common";
 
-// import { FormattedDate } from "./app.interface";
-import { AlbumsService } from "./albums/albums.service";
+import { FormattedDate } from "./app.interface";
 
 @Injectable()
 export class AppService {
-  constructor(private readonly albumsService: AlbumsService) {}
+  getHello(): FormattedDate {
+    const date = new Date();
 
-  // getHello(): FormattedDate {
-  //   const date = new Date();
-
-  //   return {
-  //     date: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
-  //   };
-  // }
-
-  getAlbums() {
-    return this.albumsService.findAll();
+    return {
+      date: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
+    };
   }
 }
