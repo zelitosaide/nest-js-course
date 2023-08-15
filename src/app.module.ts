532966c257170f5f234fc0d2d7cef35d7265cdfa
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-15 22:39:39 +0200

    Sharing resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,38 +4,25 @@ import { AppService } from "./app.service";
 import { NinjasModule } from "./ninjas/ninjas.module";
 import { CatsController } from "./cats/cats.controller";
 import { CatsService } from "./cats/cats.service";
-// import { ArtistsController } from "./artists/artists.controller";
-// import { ArtistsService } from "./artists/artists.service";
-// import { AlbumsController } from "./albums/albums.controller";
-// import { AlbumsService } from "./albums/albums.service";
 import { SongsController } from "./songs/songs.controller";
 import { SongsService } from "./songs/songs.service";
-// import { PlaylistsController } from "./playlists/playlists.controller";
-// import { PlaylistsService } from "./playlists/playlists.service";
 import { PlaylistSongsController } from "./playlist_songs/playlist_songs.controller";
 import { PlaylistSongsService } from "./playlist_songs/playlist_songs.service";
 import { AlbumsModule } from "./albums/albums.module";
 import { ArtistsModule } from "./artists/artists.module";
-import { PlaylistsModule } from "./playlists/playlists.module";
 
 @Module({
-  imports: [NinjasModule, AlbumsModule, ArtistsModule, PlaylistsModule],
+  imports: [NinjasModule, AlbumsModule, ArtistsModule],
   controllers: [
     AppController,
     CatsController,
-    // ArtistsController,
-    // AlbumsController,
     SongsController,
-    // PlaylistsController,
     PlaylistSongsController,
   ],
   providers: [
     AppService,
     CatsService,
-    // ArtistsService,
-    // AlbumsService,
     SongsService,
-    // PlaylistsService,
     PlaylistSongsService,
   ],
 })
