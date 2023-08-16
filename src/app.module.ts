28a635f4fdde129efb7b371cbe52aa18d1928e85
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-16 11:18:00 +0200

    Typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { NinjasModule } from "./ninjas/ninjas.module";
-import { CatsController } from "./cats/cats.controller";
-import { CatsService } from "./cats/cats.service";
 import { SongsController } from "./songs/songs.controller";
 import { SongsService } from "./songs/songs.service";
 import { PlaylistSongsController } from "./playlist_songs/playlist_songs.controller";
@@ -12,13 +9,8 @@ import { AlbumsModule } from "./albums/albums.module";
 import { ArtistsModule } from "./artists/artists.module";
 
 @Module({
-  imports: [NinjasModule, AlbumsModule, ArtistsModule],
-  controllers: [
-    AppController,
-    CatsController,
-    SongsController,
-    PlaylistSongsController,
-  ],
-  providers: [AppService, CatsService, SongsService, PlaylistSongsService],
+  imports: [AlbumsModule, ArtistsModule],
+  controllers: [AppController, SongsController, PlaylistSongsController],
+  providers: [AppService, SongsService, PlaylistSongsService],
 })
 export class AppModule {}
