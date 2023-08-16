e716fa9114ed34ab3a173f9351f43385c4abc731
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-16 12:49:03 +0200

    TypeOrm

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -29,7 +29,7 @@ export class UsersService {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number): Promise<void> {
+    await this.usersRepository.delete(id);
   }
 }
