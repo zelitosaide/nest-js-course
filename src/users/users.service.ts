2072d02ac235a2bc2cc31af71a48a48b637a3b4c
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-16 12:39:37 +0200

    TypeOrm

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,8 +2,17 @@ import { Injectable } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./entities/user.entity";
+import { Repository } from "typeorm";
+
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>
+  ) {}
+  
   create(createUserDto: CreateUserDto) {
     return "This action adds a new user";
   }
