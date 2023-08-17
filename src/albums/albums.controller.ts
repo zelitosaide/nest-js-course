98844f08bdbb9fbb74ee7ab6c2590dfdf3f89f5a
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-17 20:10:27 +0200

    Albums

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -2,7 +2,6 @@ import { Body, Controller, Get, Param, ParseIntPipe, Post } from "@nestjs/common
 import { AlbumsService } from "./albums.service";
 import { Album } from "./entities/album.entity";
 import { CreateAlbumDto } from "./dto/create-album.dto";
-// import { Album } from "./interfaces/album.interface";
 
 @Controller("albums")
 export class AlbumsController {
