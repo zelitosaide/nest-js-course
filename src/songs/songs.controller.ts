38c592c9bed55f92767af00d1f710ab6cc431278
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-17 22:27:30 +0200

    Songs

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { SongsService } from "./songs.service";
+import { Song } from "./entities/song.entity";
+import { CreateSongDto } from "./dto/create-song.dto";
 
 @Controller("songs")
-export class SongsController {}
+export class SongsController {
+  constructor(private songsService: SongsService) {}
+
+  @Post()
+  create(@Body() createSongDto: CreateSongDto): Promise<Song> {
+    return this.songsService.create(createSongDto);
+  }
+}
