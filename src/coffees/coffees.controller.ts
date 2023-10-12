415f3fc199d19d2977047d76c542680be6b4ce0c
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-12 23:49:05 +0200

    NestJS Course

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -23,27 +23,32 @@ export class CoffeesController {
     const { limit, offset } = paginationQuery;
     // response.status(200).send("This action return all coffees");
     // return `This action return all coffees. Limit: ${limit}, offset: ${offset}`;
-    return paginationQuery;
+    // return paginationQuery;
+    return this.coffeesService.findAll();
   }
 
   @Get(":id")
   findOne(@Param("id") id: string) {
-    return `This action returns #${id} coffee`;
+    // return `This action returns #${id} coffee`;
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
   // @HttpCode(HttpStatus.GONE)
   create(@Body() body) {
-    return body;
+    // return body;
+    return this.coffeesService.create(body);
   }
 
   @Patch(":id")
   update(@Param("id") id: string, @Body() body) {
-    return { id, body };
+    // return { id, body };
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(":id")
   remove(@Param("id") id: string) {
-    return `This action removes #${id} coffee`;
+    // return `This action removes #${id} coffee`;
+    return this.coffeesService.remove(id);
   }
 }
