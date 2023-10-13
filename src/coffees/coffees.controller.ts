d0d9aeeea329e3f3999867501333ee72f0432d42
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-13 07:48:22 +0200

    NestJS Course

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -13,6 +13,7 @@ import {
 } from "@nestjs/common";
 import { CoffeesService } from "./coffees.service";
 import { CreateCoffeeDto } from "./dto/create-coffee.dto";
+import { UpdateCoffeeDto } from "./dto/update-coffee.dto";
 
 @Controller("coffees")
 export class CoffeesController {
@@ -42,9 +43,9 @@ export class CoffeesController {
   }
 
   @Patch(":id")
-  update(@Param("id") id: string, @Body() body) {
+  update(@Param("id") id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
     // return { id, body };
-    return this.coffeesService.update(id, body);
+    return this.coffeesService.update(id, updateCoffeeDto);
   }
 
   @Delete(":id")
