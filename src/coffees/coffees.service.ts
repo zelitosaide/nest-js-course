d0d12852ed20149634072b0951a31485bd54e0ad
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-13 08:22:33 +0200

    NestJS Course

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -33,6 +33,7 @@ export class CoffeesService {
 
   create(createCoffeeDto: any) {
     this.coffees.push(createCoffeeDto);
+    return createCoffeeDto;
   }
 
   update(id: string, updateCoffeeDto: any) {
