c945023f5ef907ce99d1fd825324b9a82b821759
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-15 15:41:35 +0200

    NestJS Course

--- src/common/dto/pagination-query.dto.ts
+++ src/common/dto/pagination-query.dto.ts
@@ -1,9 +1,14 @@
-import { Type } from "class-transformer";
+// import { Type } from "class-transformer";
+import { IsOptional, IsPositive } from "class-validator";
 
 export class PaginationQueryDto {
-  @Type(() => Number)
+  @IsOptional()
+  @IsPositive()
+  // @Type(() => Number)
   limit: number;
   
-  @Type(() => Number)
+  @IsOptional()
+  @IsPositive()
+  // @Type(() => Number)
   offset: number;
 }
