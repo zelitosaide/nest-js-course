9796378f0c484e412ca7c3691d4141a9dd02b31c
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-15 16:37:47 +0200

    NestJS Course

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -94,5 +94,13 @@ export class CoffeesService {
 
     await queryRunner.connect();
     await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
