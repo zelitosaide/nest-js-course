ca5c8967c8abfaef5563f06ed83963c4ba44128b
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-16 11:50:27 +0200

    NestJS Course

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,19 @@
 import { Module } from "@nestjs/common";
 import { CoffeeRatingService } from "./coffee-rating.service";
 import { CoffeesModule } from "src/coffees/coffees.module";
+import { DatabaseModule } from "src/database/database.module";
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    DatabaseModule.register({
+      type: "postgres",
+      host: "localhost",
+      username: "postgres",
+      password: "pass123",
+      port: 5433,
+    }),
+  ],
   providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,18 +2,18 @@ import { DynamicModule, Module } from "@nestjs/common";
 import { DataSource, DataSourceOptions } from "typeorm";
 
 @Module({
-  providers: [
-    {
-      provide: "CONNECTION",
-      useValue: new DataSource({
-        type: "postgres",
-        host: "localhost",
-        username: "postgres",
-        password: "pass123",
-        port: 5433,
-      }).initialize(),
-    },
-  ],
+  // providers: [
+  //   {
+  //     provide: "CONNECTION",
+  //     useValue: new DataSource({
+  //       type: "postgres",
+  //       host: "localhost",
+  //       username: "postgres",
+  //       password: "pass123",
+  //       port: 5433,
+  //     }).initialize(),
+  //   },
+  // ],
 })
 export class DatabaseModule {
   static register(options: DataSourceOptions): DynamicModule {
