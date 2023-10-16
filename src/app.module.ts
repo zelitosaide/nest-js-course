e456dc569f7ea02257d8833141a5a9c68e35494c
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-16 13:38:03 +0200

    NestJS Course

--- src/app.module.ts
+++ src/app.module.ts
@@ -48,14 +48,14 @@ import { ConfigModule } from "@nestjs/config";
     ConfigModule.forRoot(),
     CoffeesModule,
     TypeOrmModule.forRoot({
-      type: "postgres", // type of our database
-      host: "localhost", // database host
-      port: 5433, // database host
-      username: "postgres", // username
-      password: "pass123", // user password
-      database: "postgres", // name of our database,
-      autoLoadEntities: true, // models will be loaded automatically
-      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+      type: "postgres",
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
     }),
     CoffeeRatingModule,
     DatabaseModule,
