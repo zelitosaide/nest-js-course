703ad51cf6c3b37926f2b6405cc1581d812d024d
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-16 10:57:39 +0200

    NestJS Course

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { Injectable, Module } from "@nestjs/common";
 import { CoffeesController } from "./coffees.controller";
 import { CoffeesService } from "./coffees.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
@@ -13,6 +13,14 @@ class ConfigService {}
 class DevelopmentConfigService {}
 class ProductionConfigService {}
 
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    // Do something
+    return ["buddy brew", "nescafe"];
+  }
+}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
@@ -26,7 +34,14 @@ class ProductionConfigService {}
     },
     CoffeesService,
     // { provide: COFFEE_BRANDS, useValue: ["buddy brew", "nescafe"] },
-    { provide: COFFEE_BRANDS, useFactory: () => ["buddy brew", "nescafe"] },
+    // { provide: COFFEE_BRANDS, useFactory: () => ["buddy brew", "nescafe"] },
+    CoffeeBrandsFactory,
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+        brandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
+    },
   ],
   // providers: [{ provide: CoffeesService, useValue: new MockCoffeesService() }],
   exports: [CoffeesService],
