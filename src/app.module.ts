fd436f938094331fd5a063116aad67cb7471477f
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-16 11:17:05 +0200

    NestJS Course

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,6 +19,7 @@ import { AppService } from "./app.service";
 import { CoffeesModule } from "./coffees/coffees.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { CoffeeRatingModule } from "./coffee-rating/coffee-rating.module";
+import { DatabaseModule } from "./database/database.module";
 
 @Module({
   imports: [
@@ -55,6 +56,7 @@ import { CoffeeRatingModule } from "./coffee-rating/coffee-rating.module";
       synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
     }),
     CoffeeRatingModule,
+    DatabaseModule,
   ],
   controllers: [
     AppController,
