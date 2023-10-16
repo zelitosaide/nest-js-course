bccd8b00fb49035181b108fd88c7e4e604dae69e
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-16 11:21:51 +0200

    NestJS Course

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,18 @@
 import { Module } from "@nestjs/common";
+import { DataSource } from "typeorm";
 
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: "CONNECTION",
+      useValue: new DataSource({
+        type: "postgres",
+        host: "localhost",
+        username: "postgres",
+        password: "pass123",
+        port: 5433,
+      }).initialize(),
+    },
+  ],
+})
 export class DatabaseModule {}
